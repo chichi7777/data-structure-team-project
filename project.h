e5d5bd791f320b68f463d6b4d42593b35fb54bd0
chichi7777 <96734532+chichi7777@users.noreply.github.com>
2021-12-27 23:40:44 +0800

    Add files via upload

---
+++ project.h
@@ -0,0 +1,50 @@
+typedef struct average {
+	float waitTime;
+	float respTime;
+	float turnardTime;
+} aveType;	
+
+typedef struct process {
+    int ID;
+    int arrivalTime;
+    int burstTime;
+    int remainingTime;
+    int waitingTime;
+    int turnaroundTime;
+    int responseTime;
+    int flag;
+} pType;
+
+typedef struct linklist {
+    pType * p;
+    struct linklist * Link;
+} lkType;
+
+typedef struct Queue {
+    lkType * front;
+    int count;
+    lkType * rear;
+} quType;
+
+quType * newQueue();
+void enqueue (quType * queue, pType * data_pointer);
+pType * dequeue (quType * queue);
+pType * front (quType * queue);
+void readFile (char * filename, quType * queue);
+int setReadyQ (quType * readyQ, quType * jobQ, int time);	// return 1 if data_pointer move to readyQ from jobQ, return 0 if no action	
+void setReadyQ2 (quType * readyQ, quType * jobQ, int currentTime, int elapsedTime);	//for Round Robin
+int showPrsRun (int time, pType * data_pointer ,float * utilization);
+void showPrsRun2 (int time, int elapsedTime, pType * data_pointer, float *utilization);	//for Round Robin
+void sortQ_with_remainingTime (quType * queue);
+void sortQ_with_arrivalTime (quType * queue);
+aveType printAverageTime (quType * queue); 
+void printResult (quType * queue);
+void schedule (quType * doneQ, pType * data_pointer, int currentTime);
+pType * FIFO (quType * readyQ, quType * doneQ, int time);
+int RR (quType * jobQ, quType * readyQ, quType * doneQ, int currentTime, int timeQuantum, float * utilization);
+int SJF (quType * readyQ, quType * doneQ, int time);
+pType * SRTF (quType * readyQ, quType * doneQ, int time);
+int running_with_FIFO (char * filename, quType * doneQ);
+int running_with_RR (char * filename, quType * doneQ);
+int running_with_SJF (char * filename, quType * doneQ);
+int running_with_SRTF (char * filename, quType * doneQ);
